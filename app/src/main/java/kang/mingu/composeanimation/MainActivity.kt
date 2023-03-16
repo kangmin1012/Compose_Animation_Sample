36cc6fce80d94aea28f54d274c61b2d344efdfad
kangmin1012 <kangmin1012@naver.com>
2023-03-16 18:43:13 +0900

    flipflap 애니메이션

--- app/src/main/java/kang/mingu/composeanimation/MainActivity.kt
+++ app/src/main/java/kang/mingu/composeanimation/MainActivity.kt
@@ -3,22 +3,26 @@ package kang.mingu.composeanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kang.mingu.composeanimation.ui.theme.ComposeAnimationSampleTheme
+import kang.mingu.composeanimation.ui.theme.Pink80
+import kotlin.math.abs
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,65 +30,145 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeAnimationSampleTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                ActivityView()
             }
         }
     }
 }
 
 @Composable
-fun Step(number: Int) {
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
+fun ActivityView() {
+    Surface(
+        color = Pink80
     ) {
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .clickable { flipBack() }
-        )
-
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                modifier = Modifier.align(Alignment.Center),
-                text = number.toString(),
-                color = Color.Black,
-                fontSize = 40.sp
-            )
+            StepperAnimation(modifier = Modifier)
         }
-
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .clickable {
-                    flipNext()
-                }
-        )
     }
 }
+
+@Composable
+fun StepperAnimation(modifier: Modifier) {
+    var currentNumber by remember {
+        mutableStateOf(0)
+    }
+    var frontNumber by remember {
+        mutableStateOf(0)
+    }
+    var backNumber by remember {
+        mutableStateOf(currentNumber)
+    }
+    var targetAngle by remember {
+        mutableStateOf(0f)
+    }
+
+    val rotation = animateFloatAsState(
+        targetValue = targetAngle,
+        animationSpec = tween(
+            durationMillis = 400,
+            easing = FastOutSlowInEasing,
+        )
+    )
+
+    fun isFront(): Boolean {
+        val value = abs(targetAngle % 360)
+        return value < 90 || value > 270
+    }
+
+    fun flipBack() {
+        currentNumber -= 1
+        if (isFront()) {
+            backNumber = currentNumber
+        } else {
+            frontNumber = currentNumber
+        }
+        targetAngle -= 180f
+    }
+
+    fun flipNext() {
+        currentNumber += 1
+        if (isFront()) {
+            backNumber = currentNumber
+        } else {
+            frontNumber = currentNumber
+        }
+        targetAngle += 180f
+    }
+
+    @Composable
+    fun Step(number: Int, rotationY: Float) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .graphicsLayer {
+                    this.rotationY = rotationY
+                }
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .clickable { flipBack() }
+            )
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+            ) {
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = number.toString(),
+                    color = Color.Black,
+                    fontSize = 40.sp
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .clickable {
+                        flipNext()
+                    }
+            )
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .height(80.dp)
+            .width(200.dp)
+            .graphicsLayer {
+                rotationY = rotation.value
+            }
+    ) {
+        if (isFront()) {
+            //Front
+            Step(number = frontNumber, rotationY = 0f)
+        } else {
+            //Back
+            Step(number = backNumber, rotationY = targetAngle)
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ActivityPreview() {
+    ComposeAnimationSampleTheme {
+        ActivityView()
+    }
+}
+
 
 @Preview
 @Composable
 fun StepPreview() {
-    Step(number = 1)
-}
-
-fun flipBack() {
-
-}
-
-fun flipNext() {
-
+    StepperAnimation(Modifier)
 }
